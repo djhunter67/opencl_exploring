d7a9e76cc26b673cbcde2129671c60a250d46ad6
Christerpher Hunter <djhunter67@gmail.com>
2025-04-05 11:13:00 -0400

    wip: intel graphics

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("\nPlatform: {}", platform.name()?);
         Device::list_all(platform)?.iter().for_each(|device| {
             println!(
-                "\tDevice:{}",
+                "\t\tDevice:{}",
                 device.name().expect("Failed to get device name")
             );
             println!(
@@ -45,31 +45,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // }
     }
 
-    // trivial()?;
+    trivial()?;
 
     Ok(())
 }
 fn trivial() -> ocl::Result<()> {
-    let src = r"
+    static KERNEL_SRC: &str = r"
         __kernel void add(__global float* buffer, float scalar) {
             buffer[get_global_id(0)] += scalar;
-        }
-    ";
+        }";
+
+    // let src = include_str!("./assets/scaler_mul.cl");
 
     // (1) Define which platform and device(s) to use. Create a context,
     // queue, and program then define some dims (compare to step 1 above).
-    let platform = Platform::default();
-    println!("Using platform: {}", platform.name()?);
+
+    let platforms = Platform::list();
+
     println!(
-        "\tAll platforms: {}",
-        Platform::list()
+        "\n\nAll platforms:\n{}",
+        platforms
             .iter()
             .map(|p| p.name().expect("Failed to get platform name"))
             .collect::<Vec<_>>()
-            .join(", ")
+            .join("\n")
     );
 
-    let device = Device::first(platform)?;
+    let platform = platforms
+        .into_iter()
+        .find(|p| p.name().expect("No name").contains("NVIDIA"))
+        .expect("No NVIDIA platform found");
+
+    // let platform = Platform::list()
+    //     .into_iter()
+    //     .find(|p| p.name().expect("No name").contains("Intel"))
+    //     .expect("No Intel platform found");
+
+    println!("\n\nUsing platform: {}", platform.name()?);
+
+    let list_all = Device::list_all(platform)?;
+    let device = list_all.first().expect("No devices found");
+
     println!("Using device: {}", device.name()?);
     let context = Context::builder()
         .platform(platform)
@@ -77,10 +93,11 @@ fn trivial() -> ocl::Result<()> {
         .build()?;
     let program = Program::builder()
         .devices(device)
-        .src(src)
+        .src(KERNEL_SRC)
         .build(&context)?;
-    let queue = Queue::new(&context, device, None)?;
-    let dims = 1 << 20;
+    let queue = Queue::new(&context, *device, None)?;
+    // let dims = 1 << 20; //1 << 20 = 2^20 = 1048576
+    let dims = 1 << 7;
     // [NOTE]: At this point we could manually assemble a ProQue by calling:
     // `ProQue::new(context, queue, program, Some(dims))`. One might want to
     // do this when only one program and queue are all that's needed. Wrapping
@@ -101,7 +118,7 @@ fn trivial() -> ocl::Result<()> {
         .queue(queue.clone())
         .global_work_size(dims)
         .arg(&buffer)
-        .arg(10.0)
+        .arg(10.0f32)
         .build()?;
 
     // (4) Run the kernel (default parameters shown for demonstration purposes):
@@ -120,9 +137,6 @@ fn trivial() -> ocl::Result<()> {
     buffer.cmd().queue(&queue).offset(0).read(&mut vec).enq()?;
 
     // Print an element:
-    println!(
-        "The value at index [{}] is now '{}'!",
-        200_007, vec[200_007]
-    );
+    println!("The value at index [{}] is now '{}'!", 27, vec[27]);
     Ok(())
 }
